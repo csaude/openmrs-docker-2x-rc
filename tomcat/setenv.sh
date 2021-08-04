35bd7597a2ca6069d6b02b1f84a03c53a16d4374
guimino <minoneves@gmail.com>
2021-08-04 13:44:25 +0200

    moved back openmrs-runtime.properties to tomcat folder rather injecting via binding

--- tomcat/setenv.sh
+++ tomcat/setenv.sh
@@ -1,2 +1,2 @@
-export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=768m -Duser.timezone=Africa/Maputo"
-export OPENMRS_RUNTIME_PROPERTIES_FILE='/usr/local/tomcat/.OpenMRS/openmrs-runtime.properties'
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Xms512m -Xmx1536m -XX:PermSize=768m -XX:MaxPermSize=1024m -Duser.timezone=Africa/Maputo"
+export OPENMRS_RUNTIME_PROPERTIES_FILE='/usr/local/tomcat/openmrs-runtime.properties'
