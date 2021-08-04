f576983977a2fe3580185dcc5c73d1f7fdf2a1a7
guimino <minoneves@gmail.com>
2021-08-04 06:31:04 +0200

    binding catalina log, openmrs-runtime.properties and tunning mysql container

---
+++ tomcat/setenv.sh
@@ -0,0 +1,2 @@
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=768m -Duser.timezone=Africa/Maputo"
+export OPENMRS_RUNTIME_PROPERTIES_FILE='/usr/local/tomcat/.OpenMRS/openmrs-runtime.properties'
